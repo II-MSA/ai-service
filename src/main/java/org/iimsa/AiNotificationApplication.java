ee3d19fd2436ef80a6d345a90c1efde5800ef407
rkdms6767 <rkdms6767@naver.com>
2026-03-31 10:09:04 +0900

    chore: 프로젝트 초기 환경 설정

--- src/main/java/org/iimsa/AiNotificationApplication.java
+++ src/main/java/org/iimsa/AiNotificationApplication.java
@@ -1,13 +1,13 @@
-package org.iimsa.aiservice;
+package org.iimsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AiServiceApplication {
+public class AiNotificationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AiServiceApplication.class, args);
+        SpringApplication.run(AiNotificationApplication.class, args);
     }
 
 }
